d578d939ef7ca65d846f8524e5fb95c90b68d65a
SarayRivas <ss.rivas@uniandes.edu.co>
2025-04-06 18:46:25 -0500

    final

--- Serie.ts
+++ Serie.ts
@@ -7,13 +7,13 @@ export class Serie {
     link: string;
     image: string;
 
-    constructor(id: number, nombre: string, plataforma: string, temporadas: number, sinopsis: string, link: string, imagen: string) {
+    constructor(id: number, title: string, platform: string, seasons: number, description: string, link: string, image: string) {
         this.id = id;
-        this.title = nombre;
-        this.platform = plataforma;
-        this.seasons = temporadas;
-        this.description = sinopsis;
+        this.title = title;
+        this.platform = platform;
+        this.seasons = seasons;
+        this.description = description;
         this.link = link;
-        this.image = imagen;
+        this.image = image;
     }
 }
